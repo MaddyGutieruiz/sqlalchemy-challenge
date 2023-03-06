902e36725c5b1aee7b7859d9622d58bf7a76b96a
MaddyGutieruiz <119770462+MaddyGutieruiz@users.noreply.github.com>
2023-03-05 22:40:38 -0800

    Update Part2_ClimateApp.py

--- Part2_ClimateApp.py
+++ Part2_ClimateApp.py
@@ -1,8 +1,3 @@
-import datetime as dt
-from dateutil.relativedelta import relativedelta
-import numpy as np
-import pandas as pd
-
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
